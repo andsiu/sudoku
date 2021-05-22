99386cfb713078adbfb200981ec6170734943291
andsiu <andrewsiu@shaw.ca>
2021-05-22 14:58:44 -0700

    added clear button. basically done.

--- src/appHelper.java
+++ src/appHelper.java
@@ -7,14 +7,21 @@ import java.util.ArrayList;
 public class appHelper extends JFrame {
     ArrayList<JTextField> bigList = new ArrayList<>();
     private JButton button1 = new JButton("Solve");
+    private JButton button2 = new JButton("Clear");
     private sudoku sudokuSubject = new sudoku();
 
 
-    public void buttonPressed() {
+    public void solveButtonPressed() {
         scanIntoCustomBoard();
         sudokuSubject.solve();
         displayOntoTextField();
-        sudokuSubject.display();
+        //sudokuSubject.display();
+    }
+
+    public void clearButtonPressed() {
+        for (int i = 0; i <= 80; i++) {
+            bigList.get(i).setText("");
+        }
     }
 
     public void scanIntoCustomBoard() {
@@ -23,12 +30,12 @@ public class appHelper extends JFrame {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 String value = bigList.get(temp).getText();
-                System.out.println("the value is " + value);
+                //System.out.println("the value is " + value);
                 if (value.equals("") || Integer.parseInt(bigList.get(temp).getText()) > 9) {
                     s[i][j] = 0;
                 } else {
                     s[i][j] = Integer.parseInt(bigList.get(temp).getText());
-                    System.out.println("running else!");
+                    //System.out.println("running else!");
                 }
                 temp++;
             }
@@ -54,21 +61,23 @@ public class appHelper extends JFrame {
             frame.getContentPane().add(bigList.get(i));
             bigList.get(i).setPreferredSize(new Dimension( 66, 35 ) );
             //System.out.println("finished creating jtextfield " + i);
-            if (temp < 3) {
-                bigList.get(i).setBackground(Color.GREEN);
-                temp++;
-            } else if (temp == 5) {
-                temp = 0;
-            } else {
-                temp++;
-            }
+            //script for drawing color here
+
         }
-        button1.setPreferredSize(new Dimension(450, 24));
+        button1.setPreferredSize(new Dimension(225, 24));
         frame.getContentPane().add(button1);
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                buttonPressed();
+                solveButtonPressed();
+            }
+        });
+        button2.setPreferredSize(new Dimension(225, 24));
+        frame.getContentPane().add(button2);
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearButtonPressed();
             }
         });
 
