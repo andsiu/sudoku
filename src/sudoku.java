0a5156bcc9808cfbd14a66632a09595395f90b9b
andsiu <andrewsiu@shaw.ca>
2021-05-21 19:14:07 -0700

    added variables and gui and names

--- src/sudoku.java
+++ src/sudoku.java
@@ -11,6 +11,22 @@ public class sudoku {
             { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
             { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
     };
+
+    int a0, a1, a2, a3, a4, a5, a6, a7, a8, b0, b1, b2, b3, b4, b5, b6, b7, b8, c0, c1, c2, c3, c4, c5, c6, c7, c8,
+            d0, d1, d2, d3, d4, d5, d6, d7, d8, e0, e1, e2, e3, e4, e5, e6, e7, e8, f0, f1, f2, f3, f4, f5, f6, f7, f8,
+            g0, g1, g2, g3, g4, g5, g6, g7, g8, h0, h1, h2, h3, h4, h5, h6, h7, h8, i0, i1, i2, i3, i4, i5, i6, i7, i8;
+
+    int[][] customBoard = {
+            { a0, a1, a2, a3, a4, a5, a6, a7, a8 },
+            { b0, b1, b2, b3, b4, b5, b6, b7, b8 },
+            { c0, c1, c2, c3, c4, c5, c6, c7, c8 },
+            { d0, d1, d2, d3, d4, d5, d6, d7, d8 },
+            { e0, e1, e2, e3, e4, e5, e6, e7, e8 },
+            { f0, f1, f2, f3, f4, f5, f6, f7, f8 },
+            { g0, g1, g2, g3, g4, g5, g6, g7, g8 },
+            { h0, h1, h2, h3, h4, h5, h6, h7, h8 },
+            { i0, i1, i2, i3, i4, i5, i6, i7, i8 }
+    };
     //sudoku board is a 9x9 board.
     int boardDimension = 9;
 
